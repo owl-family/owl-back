8a757abd949bc1c238fd0abde605e233754876f1
kosy318 <77595685+kosy318@users.noreply.github.com>
2022-12-29 00:39:54 +0900

    Revert "Codereview"

--- owl-back/src/main/java/com/project/owlback/codereview/dto/CodeReviewDto.java
+++
@@ -1,7 +0,0 @@
-package com.project.owlback.codereview.dto;
-
-import lombok.Data;
-
-@Data
-public class CodeReviewDto {
-}
--- owl-back/src/main/java/com/project/owlback/codereview/repository/CodeHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.owlback.codereview.repository;
-
-import com.project.owlback.codereview.model.CodeHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CodeHistoryRepository extends JpaRepository<CodeHistory, Long> {
-}
